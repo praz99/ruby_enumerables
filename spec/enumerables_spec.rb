fd21e02091977302310ae31add073bce58036fca
Hasan Ozovali <ozovalihasan@gmail.com>
2020-06-03 16:30:37 +0300

    Add test for #my_each_with_index and fix the test for #my_each

--- spec/enumerables_spec.rb
+++ spec/enumerables_spec.rb
@@ -1,17 +1,33 @@
-require '../enumerables'
+require_relative '../enumerables'
 
 describe Enumerable do
   let(:my_arr) {[1, 2, 3]}
   let(:my_hash) {{first: 1, second: 2, third: 3}}
   let(:my_range) {(0..5)}
+  let(:got_arr) {[]}
+  let(:got_arr2) {[]}
+  
 
-  describe "#my_each" do
+  describe '#my_each' do
     it 'returns each elements of the array if block is given' do
-      expect(my_arr.my_each {|x| x}).to eql(my_arr)
+      expect(my_arr.my_each {|x| got_arr << x}).to eql(got_arr)
     end
 
-    it 'returns each value and keys of the hash if block is given.' do
-      expect(my_hash.my_each {|key, value| key[value]}).to eql(my_hash)
+    it "returns each value and keys of the range if block isn't given." do
+      expect(my_arr.my_each.class).to eql(Enumerator)
     end
   end
+
+  describe '#my_each_with_index' do
+    it 'returns each elements of the array with index if block is given' do
+      expect(my_arr.my_each_with_index {|value, index| got_arr2 << value}).to eql(got_arr2)
+    end
+
+    it "returns each value and keys of the range if block isn't given." do
+      expect(my_arr.my_each_with_index.class).to eql(Enumerator)
+    end
+  end
+
+
+
 end
